6cd0906111ca551dac9710a42357e27e857b5ecc
Chris Green <chris.green@csiro.au>
2019-07-02 16:12:06 +1000

    Refactor pyexodiff and tests to pass differences

--- run_tests.py
+++ run_tests.py
@@ -2,8 +2,8 @@
 
 import pytest
 import os
+import subprocess
 import yaml
-from pyexodiff import exodiff
 
 # Exception class to throw exception for pytest
 class pyexodiffException(Exception):
@@ -45,38 +45,25 @@ for test in tests_files:
 # Run all tests found during search
 @pytest.mark.parametrize('key', tests)
 def test_pyexodiff(key):
-    ''' Run all pyexodiff tests using appropriate test function '''
-
-    # If the type key isn't specified, skip test
-    if 'type' not in tests[key].keys():
-        pytest.skip(key + ': Skipped as test type not specified')
+    ''' Run all pyexodiff tests '''
 
     # If the file key isn't specified, skip test
-    if 'file' not in tests[key].keys():
-        pytest.skip(key + ': Skipped as file not specified')
+    if 'file1' not in tests[key].keys():
+        pytest.skip(key + ': Skipped as file1 not specified')
 
-    # If the test type is exodiff, run exodiff_test
-    if tests[key]['type'] == 'exodiff':
-        # If the gold key isn't specified, skip test
-        if 'gold' not in tests[key].keys():
-            pytest.skip(key + ': Skipped as gold file not specified')
-        else:
-            exodiff_test(key)
-
-    # If the test type is exception, run the expected_error test
-    elif tests[key]['type'] == 'exception':
-        # If the expected_error key isn't specified, skip test
-        if 'expected_error' not in tests[key].keys():
-            pytest.skip(key + ': Skipped as expected_error not specified')
-        else:
-            with pytest.raises(Exception) as excinfo:
-                expected_error(key)
-
-            assert tests[key]['expected_error'] in str(excinfo.value)
+    if 'file2' not in tests[key].keys():
+        pytest.skip(key + ': Skipped as file2 not specified')
 
     else:
-        # Skip unknown test type
-        pytest.skip(key + ': Skipped as unknown test type')
+        output = exodiff_test(key)
+
+        # If there is no expected error in tests, then we expect the files to be identical
+        if 'expected_error' not in tests[key].keys():
+            assert('pyexodiff: files are identical' in output)
+
+        else:
+            # Check the output to make sure that the expected error is given
+            assert(tests[key]['expected_error'] and 'pyexodiff: files are different' in output)
 
     return
 
@@ -84,24 +71,17 @@ def exodiff_test(key):
     ''' Compare Exodus II file with gold file '''
 
     filepath = tests[key]['filepath']
-    filename = tests[key]['file']
+    file1 = tests[key]['file1']
+    file2 = tests[key]['file2']
     rtol = tests[key]['rtol']
     atol = tests[key]['atol']
-    testfilename = os.path.join(filepath, filename)
-    gold_filename = os.path.join(filepath, 'gold', tests[key]['gold'])
+    f1 = os.path.join(filepath, file1)
+    f2 = os.path.join(filepath, file2)
 
     try:
-        exodiff(testfilename, gold_filename, rtol, atol)
+        output = subprocess.check_output(['./pyexodiff.py', f1, f2]).decode('utf-8')
 
-    except:
-        with pytest.raises(Exception) as excinfo:
-            exodiff(testfilename, gold_filename, rtol, atol)
+    except subprocess.CalledProcessError:
+        raise pyexodiffException('pyexodiff failed to run')
 
-        assert tests[key]['expected_error'] in str(excinfo.value)
-
-    return
-
-def expected_error(key):
-    ''' Raise an exception when an error is thrown while pyexodiff is running '''
-
-    return
+    return output
